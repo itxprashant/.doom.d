e35886da24003532379b68992d28ea3d4371cff8
Prashant Tiwari <prashantt492@gmail.com>
2024-01-23 20:44:58 +0530

    Discard `elfeed` old configs

--- custom.el
+++ custom.el
@@ -3,10 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elfeed-feeds '("https://protesilaos.com/master.xml"))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(org-agenda-files
-   '("~/workspace/academic/mains_extra/physics/meter_bridge_theory.org" "/home/prashantt492/temp/org-latex-preview-benchmark/test-org/test-0660.org"))
  '(org-safe-remote-resources
    '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-bigblow\\.setup\\'" "\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages '(yasnippet))
